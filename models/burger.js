1ae4196ddcbc2d7bf69d4ab08c93f6faac04592d
Jim Chung <jchung225@gmail.com>
2020-01-04 20:03:40 -0500

    added modles

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,26 @@
 var orm = require('../config/orm.js');
 
-var burger_call = {
-  insert: function(some_val, cb){
-    orm.insert('burgers_tbl', 'burger_name', some_val, function(res){
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res){
       cb(res);
     });
   },
-  read: function(cb){
-    orm.read('burgers_tbl', function(res){
+  insertOne: function(cols,vals, cb){
+    orm.insertOne("burgers", cols, vals, function(res){
       cb(res);
     });
   },
-  update: function(user_id, cb){
-    orm.update('burgers_tbl', 'devoured', 1 , 'id', user_id, function(res){
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res){
       cb(res);
     });
   },
-  delete: function(user_id, cb){
-    orm.delete('burgers_tbl', 'id', user_id, function(res){
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res){
       cb(res);
     });
   }
-}
+};
 
-module.exports = burger_call;
+module.exports= burger;
