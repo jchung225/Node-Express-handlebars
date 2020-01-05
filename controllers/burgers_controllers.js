765202491adcf1b6a7b0df30e9b81f39b856c234
Jim Chung <jchung225@gmail.com>
2020-01-04 19:53:59 -0500

    controller updated

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -17,3 +17,27 @@ router.post("/api/burgers", function(req,res){
     res.json({id: result.insertId});
   });
 });
+
+router.put("/api/burgers/:id", function(req,res){
+  var condition = "id =" + req.params.id;
+  console.log("conditions", condition);
+  burger.updateOne({ devoured: req.body.devoured}, condition, function(result){
+    if (result.changedRows ===0) {
+      return res.status(404).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req,res){
+  var condition = "id =" + req.params.id;
+  console.log("condition", condition);
+
+  burger.deleteOne(condition, function(req, res){
+    if (result.changedRows ===0){
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+module.exports= router;
