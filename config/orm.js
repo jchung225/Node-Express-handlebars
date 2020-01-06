de9699e2b99f0a9a3050f4be5033fe10928fc02d
Jim Chung <jchung225@gmail.com>
2020-01-05 21:24:23 -0500

    new

--- config/orm.js
+++ config/orm.js
@@ -1,64 +1,82 @@
-var connection = require('../config/connection.js');
+var connection = require('./connection.js');
+
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+
+
 
 var orm = {
-  selectAll: function(table, cb) {
-      var queryString = "SELECT * FROM " + table + ";";
 
-      connection.query(queryString, function(err, result) {
-          if (err) {
-              throw err;
-          }
-          cb(result);
-      });
+  selectAll: function(callback) {
+
+
+    connection.query('SELECT * FROM burgers', function (err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+
   },
-  insertOne: function(table, cols, vals, cb) {
-      var queryString = "INSERT INTO " + table;
-      queryString += " (";
-      queryString += cols.toString();
-      queryString += ") ";
-      queryString += "VALUES (";
-      queryString += printQuestionMarks(vals.length);
-      queryString += ") ";
 
-      console.log(queryString);
+  insertOne: function(burger_name, callback){
 
-      connection.query(queryString, vals, function(err, result) {
-          if (err) {
-              throw err
-          }
-          cb(result);
-      });
+    var d = new Date();
+    var timestamp = ''+ d.getFullYear() + '-'; 
+    var month = '' + (d.getMonth() + 1); 
+      if(month.length == 1){
+        month = '0' + month;
+      }
+    timestamp += month + '-';
+    var day = '' + d.getDate(); 
+      if(day.length == 1){
+        day = '0' + day;
+      }
+    timestamp += day + ' ';
+    var hour = '' + d.getHours(); 
+      if(hour.length == 1){
+        hour = '0' + hour;
+      }
+    timestamp += hour + ':';
+    var minute = '' + d.getMinutes(); 
+      if(minute.length == 1){
+        minute = '0' + minute;
+      }
+    timestamp += minute + ':';
+    var second = '' + d.getSeconds(); 
+      if(second.length == 1){
+        second = '0' + second;
+      }
+    timestamp += second;
+
+    connection.query('INSERT INTO burgers SET ?', {
+      burger_name: burger_name,
+      devoured: false,
+      date: timestamp
+    }, function (err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+
   },
-  updateOne: function(table, objColVals, condition, cb) {
-      var queryString = "UPDATE " + table;
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
 
-      console.log(queryString);
+  updateOne: function(burgerID, callback){
 
-      connection.query(queryString, function(err, result) {
-          if (err) {
-              throw err
-          }
-          cb(result);
+    connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
+        if (err) throw err;
+        callback(result);
       });
-  },
-  deleteOne: function(table, condition, cb) {
-      var queryString = "DELETE FROM " + table;
-      queryString += " WHERE ";
-      queryString += condition;
 
-      console.log(queryString);
-
-      connection.query(queryString, function(err, result) {
-          if (err) {
-              throw err
-          }
-          cb(result);
-      });
   }
+
 };
+
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path')
+var methodOverride = require('method-override')
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static("public"));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
 
-app.use(bodyParser.json());
+var router = require('./controllers/burgers_controllers.js');
+app.use('/', router);
 
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
-app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgers_controllers.js");
-
-app.use(routes);
-
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT)
-});
+var port = process.env.PORT || 3000;
+app.listen(port);
